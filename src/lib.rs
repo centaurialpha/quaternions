646976ddafb62d25aa18965c75e12625af42d33c
Gabriel Acosta <acostadariogabriel@gmail.com>
2024-03-09 21:36:42 -0300

    add conjugate, inverse, square norm

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Div, Mul};
 
 #[derive(Debug)]
 pub struct Quaternion {
@@ -20,6 +20,26 @@ impl Quaternion {
     fn imaginary(&self) -> (f64, f64, f64) {
         (self.qi, self.qj, self.qk)
     }
+    fn square_norm(&self) -> f64 {
+        self.qr * self.qr + self.qi * self.qi + self.qj * self.qj + self.qk * self.qk
+    }
+
+    fn conjugate(&self) -> Self {
+        Quaternion::new(self.qr, -self.qi, -self.qj, -self.qk)
+    }
+
+    fn inverse(&self) -> Self {
+        // FIXME: pfffffffff
+        let q_conjugate = self.conjugate();
+        let inverse_scalar = 1.0 / self.square_norm();
+
+        Quaternion::new(
+            q_conjugate.qr * inverse_scalar,
+            q_conjugate.qi * inverse_scalar,
+            q_conjugate.qj * inverse_scalar,
+            q_conjugate.qk * inverse_scalar,
+        )
+    }
 }
 
 impl Add for Quaternion {
@@ -34,6 +54,34 @@ impl Add for Quaternion {
     }
 }
 
+impl Mul for Quaternion {
+    type Output = Quaternion;
+    fn mul(self, other: Quaternion) -> Quaternion {
+        let qr = self.qr * other.qr - self.qi * other.qi - self.qj * other.qj - self.qk * other.qk;
+        let qi = self.qr * other.qi + self.qi * other.qr + self.qj * other.qk - self.qk * other.qj;
+        let qj = self.qr * other.qj - self.qi * other.qk + self.qj * other.qr + self.qk * other.qi;
+        let qk = self.qr * other.qk + self.qi * other.qj - self.qj * other.qi + self.qk * other.qr;
+        Quaternion::new(qr, qi, qj, qk)
+    }
+}
+
+impl Mul<f64> for Quaternion {
+    type Output = Quaternion;
+    fn mul(self, p: f64) -> Self::Output {
+        Quaternion::new(self.qr * p, self.qi * p, self.qj * p, self.qk * p)
+    }
+}
+impl Div<Quaternion> for f64 {
+    type Output = Quaternion;
+    fn div(self, other: Quaternion) -> Self::Output {
+        Quaternion::new(
+            self / other.qr,
+            -self / other.qi,
+            -self / other.qj,
+            -self / other.qk,
+        )
+    }
+}
 impl PartialEq for Quaternion {
     fn eq(&self, other: &Self) -> bool {
         self.qr == other.qr && self.qi == other.qi && self.qj == other.qj && self.qk == other.qk
@@ -72,5 +120,24 @@ mod tests {
         let q1 = Quaternion::new(0.1, 0.2, 0.3, 0.4);
         let q2 = Quaternion::new(0.0, 0.3, 0.1, 0.2);
         assert_eq!(q1 + q2, Quaternion::new(0.1, 0.5, 0.4, 0.6000000000000001));
+    }
+    #[test]
+    fn conjugate() {
+        let q = Quaternion::new(0.1, 0.2, 0.3, 0.3);
+        let q_conjugate = q.conjugate();
+        let q_expected = Quaternion::new(0.1, -0.2, -0.3, -0.3);
+        assert_eq!(q_conjugate, q_expected);
+    }
+    #[test]
+    fn inverse() {
+        let q = Quaternion::new(1.0, 2.0, 3.0, 4.0);
+        let q_inverse = q.inverse();
+        let q_expected = Quaternion::new(
+            0.18257418583505539,
+            -0.36514837167011077,
+            -0.5477225575051662,
+            -0.7302967433402215,
+        );
+        assert_eq!(q_inverse, q_expected);
     }
 }
